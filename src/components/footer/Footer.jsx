5e4b9ea00ceb5d8659c1caa469f3adf93ae1f77b
Grégory Marini <greg.marini@hotmail.fr>
2023-06-25 12:20:00 +0200

    add social icons in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Footer = () => {
@@ -5,7 +6,36 @@ const Footer = () => {
   return (
     <div className='h-[50px] flex items-center justify-between'>
       <div>&copy;2023 Sarah. Tous droits réservés.</div>
-      <div></div>
+      <div className='flex gap-2 opacity-60'>
+        <Image
+          src='/assets/socialIcons/discord.jpg'
+          width={20}
+          height={20}
+          alt='Sarah portfolio discord icon'
+          className='cursor-pointer'
+        />
+        <Image
+          src='/assets/socialIcons/facebook.png'
+          width={20}
+          height={20}
+          alt='Sarah portfolio facebook icon'
+          className='cursor-pointer'
+        />
+        <Image
+          src='/assets/socialIcons/instagram.jpg'
+          width={20}
+          height={20}
+          alt='Sarah portfolio instagram icon'
+          className='cursor-pointer'
+        />
+        <Image
+          src='/assets/socialIcons/linkedin.png'
+          width={20}
+          height={20}
+          alt='Sarah portfolio linkedin icon'
+          className='cursor-pointer'
+        />
+      </div>
     </div>
   );
 };
