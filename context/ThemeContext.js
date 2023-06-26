7502c7c9a645794381bba28f518732e41ab2b2d6
Grégory Marini <greg.marini@hotmail.fr>
2023-06-26 22:14:55 +0200

    add context for dark/light theme

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -1,5 +1,16 @@
 'use client';
 
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [mode, setMode] = useState('dark');
+  const toggle = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  <ThemeContext.Provider value={(toggle, mode)}>
+    <div className={`theme ${mode}`}>{children}</div>
+  </ThemeContext.Provider>;
+};
