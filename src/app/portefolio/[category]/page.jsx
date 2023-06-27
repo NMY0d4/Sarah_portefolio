9faabf1c1661ee7d08036864b3e1aadf53ef6bcb
Grégory Marini <greg.marini@hotmail.fr>
2023-06-27 14:43:05 +0200

    apply fake data(portfolio)

--- src/app/portefolio/[category]/page.jsx
+++ src/app/portefolio/[category]/page.jsx
@@ -1,41 +1,47 @@
 import Button from '@/components/ui/button/Button';
 import SecBtn from '@/components/ui/secBtn/SecBtn';
 import Image from 'next/image';
+import { items } from './data.js';
 import React from 'react';
+import { notFound } from 'next/navigation';
 
-// Quand mapage des éléments importé !!! pour l'effet reverse -- utiliser:
-// {votreListe.map((item, index) => (
-//   <div
-//     key={index}
-//     className={`${
-//       index % 2 === 1 ? 'flex-row-reverse' : '' // Applique la classe flex-row-reverse aux éléments d'index impair
-//     }`}
-//   >
-//     {/* Contenu de l'élément */}
-//   </div>
-// ))}
-/////////////////////////////////////////
+const getData = (cat) => {
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
 
 function Category({ params }) {
-  console.log(params);
+  const data = getData(params.category);
   return (
     <div>
       <h1 className='text-primary text-[50px]'>{params.category}</h1>
-      <div className='flex gap-12 mt-12 mb-[100px]'>
-        <div className='flex-1 flex flex-col gap-5 justify-center'>
-          <h2 className='text-[40px]'>Test</h2>
-          <p className='text-[20px]'>Description</p>
-          <SecBtn text='see More' url='#' />
+      {data.map((item, i) => (
+        <div
+          key={item.id}
+          className={`flex gap-12 mt-12 mb-[100px] ${
+            i % 2 === 1 ? 'flex-row-reverse' : ''
+          }`}
+        >
+          <div className='flex-1 flex flex-col gap-5 justify-center'>
+            <h2 className='text-[40px]'>{item.title}</h2>
+            <p className='text-[20px]'>{item.description}</p>
+            <SecBtn text='see More' url='#' />
+          </div>
+          <div className='relative flex-1 h-[500px]'>
+            <Image
+              className='object-cover'
+              src={item.image}
+              fill={true}
+              alt='Sarah portfolio project image'
+            />
+          </div>
         </div>
-        <div className='relative flex-1 h-[500px]'>
-          <Image
-            className='object-cover'
-            src='https://images.pexels.com/photos/2088208/pexels-photo-2088208.jpeg'
-            fill={true}
-            alt='Sarah portfolio project image'
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
