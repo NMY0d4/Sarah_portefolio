a10d9dac9358284ca23804e9f932e0955c4af32e
Grégory Marini <greg.marini@hotmail.fr>
2023-07-02 10:17:17 +0200

    update url(blog/[id]

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { notFound } from 'next/navigation';
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+  const res = await fetch(`${process.env.APP_URL}/api/posts/${id}`, {
     cache: 'no-store',
   });
   if (!res.ok) {
