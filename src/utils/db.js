7a34db142b619dde2e2dd9d23a8a862e2bfb55f5
Grégory Marini <greg.marini@hotmail.fr>
2023-07-06 18:24:57 +0200

    change env var name

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require('mongoose');
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URL);
+    await mongoose.connect(process.env.MONGODB_URI);
   } catch (error) {
     throw new Error('Connection failed!');
   }
